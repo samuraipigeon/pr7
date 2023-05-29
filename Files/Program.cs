d4c70ebb940ccd2fe96586accc82ecea5844aac3
Ulianets_Yulia <118103514+samuraipigeon@users.noreply.github.com>
2023-05-29 22:23:16 +0300

    Добавьте файлы проекта.

---
+++ Files/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace file
+{
+
+    class MyPage
+    {
+        public static string HighAndLow(string numbers)
+        {
+            var list = numbers.Split(' ').ToList();
+            var max = list.Select(int.Parse).Max();
+            var min = list.Select(int.Parse).Min();
+            return max + " " + min;
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            File.WriteAllText("D://new_file.txt", "414 490 145 443 17 404 123 144 445 158 291 315 159 389 99 330 149 160 225 423");
+            string[] lines = File.ReadAllLines(@"D://new_file.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+                Console.WriteLine("Максимальне і мінімальне значення:\n"+HighAndLow(line));
+            }
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Files/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Files")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Files")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("11a5b98c-f22b-40c0-8b5a-c4be898dbea1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
